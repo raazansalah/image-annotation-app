5e14d950654f14a826dbcd896e3be5d1459f0970
razan.salah <razan.salah@bosta.co>
2024-12-13 15:14:33 +0200

    feat: handle draw rectangles on mouse up and down event

--- src/app/canvas.js
+++ src/app/canvas.js
@@ -1,12 +1,16 @@
-// components/Canvas.js
 "use client"; // Ensure this is a client-side component
 
 import React, { useRef, useState, useEffect } from "react";
 
 const Canvas = ({ imageSrc }) => {
   const canvasRef = useRef(null);
-
   const canvasContext = useRef(null);
+
+  const [rectangles, setRectangles] = useState([]);
+  const [drawingParams, setDrawingParams] = useState({
+    x: 0,
+    y: 0,
+  });
 
   // Load the image and set up canvas context
   useEffect(() => {
@@ -25,13 +29,63 @@ const Canvas = ({ imageSrc }) => {
         // Clear and Draw the image onto the canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+        // Draw existing rectangles with annotations
+        if (rectangles.length) {
+          rectangles.forEach(({ x, y, width, height, text }) => {
+            ctx.beginPath();
+            ctx.rect(x, y, width, height);
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "red";
+            ctx.stroke();
+            ctx.font = "14px Arial";
+            ctx.fillStyle = "red";
+            ctx.fillText(text, x + 5, y + 15);
+          });
+        }
       };
     }
-  }, [imageSrc]);
+  }, [imageSrc, rectangles]);
 
+  const handleMouseDown = ({ clientX, clientY }) => {
+    const canvas = canvasRef.current;
+    const { top, left } = canvas.getBoundingClientRect();
+    setDrawingParams({
+      x: clientX - top,
+      y: clientY - left,
+    });
+  };
+
+  const handleMouseUp = ({ clientX, clientY }) => {
+    const canvas = canvasRef.current;
+    const { top, left } = canvas.getBoundingClientRect();
+    const { x: startX, y: startY } = drawingParams;
+    //specify width and height of rectangle when mouse is up
+    const width = clientX - left - startX;
+    const height = clientY - top - startY;
+
+    setRectangles((prev) => [
+      ...prev,
+      {
+        x: startX,
+        y: startY,
+        width,
+        height,
+      },
+    ]);
+  };
+
+  console.log(rectangles);
   return (
     <div className="relative">
-      <canvas ref={canvasRef} width={800} height={600} className="border" />
+      <canvas
+        ref={canvasRef}
+        width={800}
+        height={600}
+        className="border"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      />
     </div>
   );
 };
